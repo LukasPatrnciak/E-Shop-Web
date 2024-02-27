0f9ce6f0a191520eb3f3c0847d1c2e6adaf18f7b
Lukáš Patrnčiak <lukaspatrnciakk@gmail.com>
2024-02-27 18:49:32 +0100

    second release

--- src/main/java/sk/lukaspatrnciak/eshopweb/EShopWebApplication.java
+++ src/main/java/sk/lukaspatrnciak/eshopweb/EShopWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EShopWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EShopWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EShopWebApplication.class, args);
+    }
 
 }
--- src/test/java/sk/lukaspatrnciak/eshopweb/EShopWebApplicationTests.java
+++ src/test/java/sk/lukaspatrnciak/eshopweb/EShopWebApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EShopWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
